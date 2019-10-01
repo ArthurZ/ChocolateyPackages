ef33edfdb697f39a13316956651c8fae5920f8d2
Arthur <arthurz@gmx.com>
2019-09-30 21:42:35 -0400

    Header added

--- nim/tools/ChocolateyUninstall.ps1
+++ nim/tools/ChocolateyUninstall.ps1
@@ -1,3 +1,10 @@
+# File:          ChocolateyUninstall.ps1
+# Description:   Un-installation of the Nim programming language
+# Author:        Arthur Zubarev
+# Email:         arthurz at gmx dot com
+# Revision:      0.0.2
+# Last Modified: Tue Oct 01 2019 12:20 PM UTC
+
 $ErrorActionPreference = 'Stop';
 
-Uninstall-ChocolateyZipPackage 'Nim'
+Uninstall-ChocolateyZipPackage 'nim'
